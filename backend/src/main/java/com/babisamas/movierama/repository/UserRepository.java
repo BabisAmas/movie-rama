6989fbf98d17da9b2ea886c1e732207143195bc2
babisamas <babis_ser@hotmail.com>
2023-11-01 21:51:27 +0200

    adds user authentication with spring security

--- backend/src/main/java/com/babisamas/movierama/repository/UserRepository.java
+++ backend/src/main/java/com/babisamas/movierama/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.babisamas.movierama.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
---
+++ backend/src/main/java/com/babisamas/movierama/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.babisamas.movierama.security;
+
+import com.babisamas.movierama.model.User;
+import com.babisamas.movierama.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(username, username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: " + username));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
