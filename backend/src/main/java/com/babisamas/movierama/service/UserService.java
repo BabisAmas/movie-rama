772cc1ddd48b93ce473605c7ac3ab823453eec55
babisamas <babis_ser@hotmail.com>
2023-11-01 22:46:37 +0200

    adds getLoggedInUser method in user service

--- backend/src/main/java/com/babisamas/movierama/service/UserService.java
+++ backend/src/main/java/com/babisamas/movierama/service/UserService.java
@@ -3,6 +3,9 @@ package com.babisamas.movierama.service;
 import com.babisamas.movierama.dto.UserRegistrationDTO;
 import com.babisamas.movierama.model.User;
 import com.babisamas.movierama.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -30,5 +33,17 @@ public class UserService {
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User getLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        String username = authentication.getName();
+        return userRepository.findByUsernameOrEmail(username, username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 }
