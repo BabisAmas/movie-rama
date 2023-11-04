1e7c3e504783a87a43e73bf8ee7798dfee3caeec
babisamas <babis_ser@hotmail.com>
2023-11-04 18:57:42 +0200

    adds all/no args constructors for AuthenticationRequestDTO

--- backend/src/main/java/com/babisamas/movierama/dto/AuthenticationRequestDTO.java
+++ backend/src/main/java/com/babisamas/movierama/dto/AuthenticationRequestDTO.java
@@ -1,10 +1,14 @@
 package com.babisamas.movierama.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationRequestDTO {
     private String username;
     private String password;
