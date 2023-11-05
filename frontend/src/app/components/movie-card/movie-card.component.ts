2d3b016b64f4e806ce1f7ed5eafbbc20cb075870
babisamas <babis_ser@hotmail.com>
2023-11-05 23:39:34 +0200

    adds movie-card component structure and styling

--- frontend/src/app/components/movie-card/movie-card.component.ts
+++ frontend/src/app/components/movie-card/movie-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent {
-
+  @Input() movie: any;
 }
