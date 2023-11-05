04ba1978ccd4f208ffb4ce75186e006c71cd9790
babisamas <babis_ser@hotmail.com>
2023-11-05 14:08:17 +0200

    adds login & logout functionalities

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   showModal = false;
+
+  constructor(private authService: AuthService, private messageService: MessageService) {}
+
+  get isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  logout() {
+    this.authService.logout();
+
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Logged Out',
+      detail: 'You have been logged out successfully.'
+    });
+  }
 
   toggleModal() {
     this.showModal = !this.showModal;
--- frontend/src/app/components/login-modal/login-modal.component.ts
+++ frontend/src/app/components/login-modal/login-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-login-modal',
@@ -6,18 +10,30 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./login-modal.component.scss'],
 })
 export class LoginModalComponent {
-  user = {
-    email: '',
-    password: '',
-  };
-
   @Output() closeModalEvent = new EventEmitter<void>();
+  loginForm: FormGroup;
+  display = true;
 
-  onSubmit() {
-    this.closeModal();
+  constructor(private fb: FormBuilder, private authService: AuthService, private messageService: MessageService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
-  closeModal() {
-    this.closeModalEvent.emit();
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login(username, password).subscribe({
+        next: () => {
+          this.display = false;
+          this.closeModalEvent.emit();
+          this.messageService.add({ severity: 'success', summary: 'Login Successful', detail: 'Welcome back!' });
+        },
+        error: () => {
+          this.messageService.add({ severity: 'error', summary: 'Login Failed', detail: 'Invalid credentials' });
+        }
+      });
+    }
   }
 }
