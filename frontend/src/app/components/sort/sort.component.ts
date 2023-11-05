22be7e567b84367d56f4628bfbb29427c8834a3e
babisamas <babis_ser@hotmail.com>
2023-11-05 23:03:34 +0200

    adds sort component code and styling

--- frontend/src/app/components/sort/sort.component.ts
+++ frontend/src/app/components/sort/sort.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sort',
   templateUrl: './sort.component.html',
-  styleUrls: ['./sort.component.scss']
+  styleUrls: ['./sort.component.scss'],
 })
 export class SortComponent {
+  @Output() onSort = new EventEmitter<string>();
 
+  sortOptions: SelectItem[];
+
+  constructor() {
+    this.sortOptions = [
+      { label: 'Newest First', value: 'addedDateDesc' },
+      { label: 'Oldest First', value: 'addedDateAsc' },
+      { label: 'Most Liked', value: 'mostLikes' },
+      { label: 'Most Hated', value: 'mostHates' },
+    ];
+  }
+
+  onSortChange(event: any): void {
+    this.onSort.emit(event.value);
+  }
 }
