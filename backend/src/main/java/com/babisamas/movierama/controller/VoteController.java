e384ce4a466ed11e9df6dc991d89bde98b56efb7
babisamas <babis_ser@hotmail.com>
2023-11-18 00:17:27 +0200

    adds input validation in VoteController

--- backend/src/main/java/com/babisamas/movierama/controller/VoteController.java
+++ backend/src/main/java/com/babisamas/movierama/controller/VoteController.java
@@ -2,6 +2,7 @@ package com.babisamas.movierama.controller;
 
 import com.babisamas.movierama.model.VoteType;
 import com.babisamas.movierama.service.VoteService;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,13 @@ public class VoteController {
     }
 
     @PostMapping("/vote")
-    public ResponseEntity<Void> vote(@RequestParam Long movieId, @RequestParam VoteType voteType) {
+    public ResponseEntity<Void> vote(@RequestParam @NotNull Long movieId, @RequestParam @NotNull VoteType voteType) {
         voteService.castVote(movieId, voteType);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/vote")
-    public ResponseEntity<Void> removeVote(@RequestParam Long movieId) {
+    public ResponseEntity<Void> removeVote(@RequestParam @NotNull Long movieId) {
         voteService.removeVote(movieId);
         return ResponseEntity.ok().build();
     }
